202f39dab479e554630e813f3ab4571894c0da82
vikashanandjha <mailtovikashjha@gmail.com>
2024-01-18 02:42:32 +0530

    init

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import analytics from '@react-native-firebase/analytics';
 
 export default function App() {
+
+  const logTestEvent=async () =>{
+    await analytics().logEvent("test_dummy",{"action":true}).then((data)=>console.log(data))
+    console.log("logged")
+    console.log(analytics())
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Add To Basket"
+        onPress={logTestEvent
+        }
+      />
       <StatusBar style="auto" />
     </View>
   );
